8ff377c6269ae807b8d7e9d60d4bace31e5fbed5
fadilsyy <fadilasytar@gmail.com>
2025-06-12 14:48:25 +0700

    mengecek antrian

--- Queues.cpp
+++ Queues.cpp
@@ -17,9 +17,19 @@ public:
         cout << "Enter a number: ";
         cin >> num;
         cout<< endl;
+
+        // 1. Cek apakah antrian penuh
+        if ((FRONT == 0 && REAR = max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
+        // 2. cek apakah antrian kosong
+        
     }
 };
 
 int main() {
-    
+
 }
