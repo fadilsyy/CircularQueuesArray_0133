79daaa4a51912a471aa9741d2da43cd98d00dc54
fadilsyy <fadilasytar@gmail.com>
2025-06-12 14:54:03 +0700

    membuat circullar array

--- Queues.cpp
+++ Queues.cpp
@@ -19,15 +19,27 @@ public:
         cout<< endl;
 
         // 1. Cek apakah antrian penuh
-        if ((FRONT == 0 && REAR = max - 1) || (FRONT == REAR + 1))
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
         {
             cout << "\nQueue overflow\n";
             return;
         }
 
         // 2. cek apakah antrian kosong
-        
+        if(FRONT == -1) {
+            FRONT = 0; // 2.a
+            REAR = 0; // 2.b
+        }
+        else {
+            // jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
+
 };
 
 int main() {
